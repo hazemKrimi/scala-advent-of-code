d39cd7e302192705e8cb28c1f5dc9a9cac11099f
Hazem Krimi <krimihazem1@gmail.com>
2023-08-20 22:36:39 +0100

    Day 4

---
+++ src/main/scala/DayFour.scala
@@ -0,0 +1,22 @@
+def getPairRanges(line: String) =
+  val pair = line.split(",")
+  val firstElfSection = pair.head.split("-")
+  val secondElfSection = pair.last.split("-")
+  val firstRange = (firstElfSection.head.toInt to firstElfSection.last.toInt).toList
+  val secondRange = (secondElfSection.head.toInt to secondElfSection.last.toInt).toList
+
+  (firstRange, secondRange)
+
+@main def dayFour: Unit =
+  val path: os.Path = os.pwd / "src" / "main" / "resources" / "day-four.txt"
+  val content: Seq[String] = os.read.lines(path)
+  val sumOfContainedRanges = content.map(getPairRanges).count((firstRange, secondRange) => {
+    firstRange.sliding(secondRange.length).contains(secondRange) ||
+      secondRange.sliding(firstRange.length).contains(firstRange)
+  })
+  val sumOfOverlappingRanges = content.map(getPairRanges).count((firstRange, secondRange) => {
+    firstRange.intersect(secondRange).nonEmpty
+  })
+
+  println(sumOfContainedRanges)
+  println(sumOfOverlappingRanges)
--- src/main/scala/DayThree.scala
+++ src/main/scala/DayThree.scala
@@ -1,3 +1,5 @@
+import scala.compiletime.ops.string.CharAt
+
 val lowerCaseLetters = ('a' to 'z').toList
 val upperCaseLetters = lowerCaseLetters.map(l => l.toUpper)
 
@@ -6,16 +8,22 @@ class CompartmentItem(val letter: Char):
     if (letter.isUpper) upperCaseLetters.indexOf(letter) + 27
     else lowerCaseLetters.indexOf(letter) + 1
 
-def calculatePriority(line: String): Int =
+def calculatePriorityOfChar(c: Char): Int = CompartmentItem(c).priority
+
+def calculatePriorityOfLine(line: String): Int =
   val firstCompartment = line.take(line.length / 2)
   val secondCompartment = line.takeRight(line.length / 2)
   val misplaced = firstCompartment.intersect(secondCompartment).head
 
-  CompartmentItem(misplaced).priority
+  calculatePriorityOfChar(misplaced)
+
+def calculatePriorityOfGroup(group: Seq[String]): Int = calculatePriorityOfChar(group.map(_.toSet).reduce(_ intersect _).head)
 
 @main def dayThree: Unit =
   val path: os.Path = os.pwd / "src" / "main" / "resources" / "day-three.txt"
   val content: Seq[String] = os.read.lines(path)
-  val sumOfPriorities = content.map((line) => calculatePriority(line)).reduce(_ + _)
+  val sumOfPriorities = content.map((line) => calculatePriorityOfLine(line)).reduce(_ + _)
+  val sumOfGroupPriorities = content.sliding(3, 3).map(calculatePriorityOfGroup).reduce(_ + _)
 
   println(sumOfPriorities)
+  println(sumOfGroupPriorities)
